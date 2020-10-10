6ab1dd20f84a88af80ee7977f0d46813c77d2fb9
Michaël Zasso <targos@protonmail.com>
2020-10-10 13:20:38 +0200

    refactor: use node_modules and exec documentation

--- index.js
+++ index.js
@@ -1,9 +1,41 @@
 'use strict';
 
+const fs = require('fs').promises;
+const path = require('path');
+
 const core = require('@actions/core');
-const documentation = require('documentation');
+const exec = require('@actions/exec');
 
 const entry = core.getInput('entry');
 
-core.warning(`cwd: ${process.cwd()}`);
-core.info(`entry: ${entry}`);
+(async () => {
+  const packageJson = await getPackageJson();
+  const entryFile = entry || packageJson.module || packageJson.main;
+  if (!entryFile) {
+    return core.setFailed(
+      `Missing "entry" input or package.json's "module" or "main" field`,
+    );
+  }
+
+  await exec('node', [
+    path.join(__dirname, 'node_modules/documentation/bin/documentation.js'),
+    'build',
+    entryFile,
+    '--github',
+    '--output',
+    'docs',
+    '--format',
+    'html',
+    '--sort-order',
+    'alpha',
+  ]);
+
+  const docsDir = await fs.readdir('docs');
+  core.warning(docsDir);
+})().catch((error) => {
+  core.setFailed(error);
+});
+
+async function getPackageJson() {
+  return JSON.parse(await fs.readFile('package.json', 'utf-8'));
+}
